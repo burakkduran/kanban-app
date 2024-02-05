d672235d5b59d9532fde9f6be084a4e1165b5d54
Burak <burak.duraninfo@gmail.com>
2024-02-05 07:20:44 +0300

    Update Card component with new icons and styles

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,6 +1,6 @@
 import { todos } from "@/store/initialData";
 import { GridListItem, Button, Text } from "react-aria-components";
-import { Icon } from "@adobe/react-spectrum";
+import { Pencil, Trash2 } from "lucide-react";
 
 type CardProps = {
   id?: string;
@@ -25,27 +25,22 @@ export function Card({ id, item, className, onDelete }: CardProps) {
           ≡
         </Button>
         <Button
+          aria-label="Edit Task"
+          onPress={() => {
+            console.log("Edit Task ID: " + item.id);
+          }}
+        >
+          <Pencil size={16} />
+        </Button>
+        <Button
           aria-label="Delete Task"
-          className="justify-self-end"
           onPress={() => {
             onDelete(item.id);
           }}
         >
-          <Icon>
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
-                fill="currentColor"
-              ></path>
-            </svg>
-          </Icon>
+          <Trash2 size={16} />
         </Button>
+
       </span>
     </GridListItem>
   );
